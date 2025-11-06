c4bba3852f595665d7d9385156f9600381c071b3
Anshika-tech1 <anshikavar2209@gmail.com>
2025-11-06 14:04:01 +0530

    dom mai event managment

--- JavaScript/classwork/dom/script.js
+++ JavaScript/classwork/dom/script.js
@@ -1,9 +1,30 @@
-alert("Radhey Radhey");
-console.log("Hello");
-const mainHeading=document.getElementById("title");
-mainHeading.textContent ="Radhey Radhey";
+// alert("Radhey Radhey");
+// console.log("Hello");
+// const mainHeading=document.getElementById("title");
+// mainHeading.textContent ="Radhey Radhey";
 
-const button=document.getElementById("btn");
-button.onclick=function(){
+// const button=document.getElementById("btn");
+// button.onclick=function(){
     
-}
+// }
+
+const body=document.querySelector("body");
+const box=document.createElement("div");
+const btn=document.createElement("button");
+body.appendChild(box);
+body.appendChild(btn);
+btn.textContent="Click me";
+// box.style.height="200px";
+// box.style.width="200px";
+// box.style.backgroundColor="teal";
+// btn.style.height="50px";
+// btn.style.width=" 200px";
+// btn.style.margin="10px"
+// btn.style.backgroundColor="Blue"
+// btn.style.borderRadius="10px"
+// btn.style.borderColor="White"
+box.classList.add("small-box");
+box.addEventListener("click",function(){
+    box.classList.remove("small-box");
+    box.classList.add("big-box");
+})
