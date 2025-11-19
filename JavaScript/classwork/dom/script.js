9fc17ee1875c6cbd9d82b1397de41085c33872e2
Anshika-tech1 <anshikavar2209@gmail.com>
2025-11-19 17:24:51 +0530

    internal Assgismnet task

--- JavaScript/classwork/dom/script.js
+++ JavaScript/classwork/dom/script.js
@@ -8,13 +8,13 @@
     
 // }
 
-const body=document.querySelector("body");
-const box=document.createElement("div");
-const btn=document.createElement("button");
-body.appendChild(box);
-body.appendChild(btn);
-btn.textContent="Click me";
-// box.style.height="200px";
+// const body=document.querySelector("body");
+// const box=document.createElement("div");
+// const btn=document.createElement("button");
+// body.appendChild(box);
+// body.appendChild(btn);
+// btn.textContent="Click me";
+// // box.style.height="200px";
 // box.style.width="200px";
 // box.style.backgroundColor="teal";
 // btn.style.height="50px";
@@ -23,8 +23,29 @@ btn.textContent="Click me";
 // btn.style.backgroundColor="Blue"
 // btn.style.borderRadius="10px"
 // btn.style.borderColor="White"
-box.classList.add("small-box");
-box.addEventListener("click",function(){
-    box.classList.remove("small-box");
-    box.classList.add("big-box");
-})
+// box.classList.add("small-box");
+// box.addEventListener("click",function(){
+//     box.classList.remove("small-box");
+//     box.classList.add("big-box");
+// })
+
+// const Person=function(name,age){
+//     this.name=name;
+//     this.age=age;
+// }
+// Person.prototype.introduce=function(){
+//     console.log(`Hi,my name is ${this.name} and I am ${this.age} years old`);
+// }
+
+class Person{
+    constructor(name,age){
+        this.firstName=name;
+        this.age=age;
+    }
+    introduce(){
+        console.log(`Hi,my name is ${this.firstName} and I am ${this.age} years old`);
+    }
+}
+
+const person1=new Person("Anshika",18);
+person1.introduce();
